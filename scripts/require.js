2d6b4762f011c2808d3d8a84d659a6afa5d1ea2a
Conor Kelly <ckelly@ckellydesign.net>
2015-09-19 11:41:06 -0700

    Init Backbone Views

--- scripts/require.js
+++ scripts/require.js
@@ -1,4 +1,6 @@
-require([], function () {
+require([
+	'./views/testRoot'], function (TestRoot) {
 	Protect = window.Protect || {};
 	
+	Protect.testRoot = new TestRoot({ el: $('#j-main') });
 });
---
+++ scripts/views/testRoot.js
@@ -0,0 +1,8 @@
+define([], function(){
+	var testRoot = Backbone.View.extend({
+		initialize: function() {
+			console.log("test page initialized");
+		}
+	});
+	return testRoot;
+});
